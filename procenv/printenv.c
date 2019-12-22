79b0a2b572665fbda0789b154ef79762dcde53e5
Eric Chen <ericgtkb@gmail.com>
2019-12-22 00:30:21 -0600

    Modify printenv.c

--- procenv/printenv.c
+++ procenv/printenv.c
@@ -1,8 +1,21 @@
 #include "unixprog.h"
 
+extern char **environ;
+
 int main() {
-    for (int i = 0; __environ[i] != NULL; i++) {
-        printf("%s\n", __environ[i]);
+
+    for (int i = 0; environ[i] != NULL; i++) {
+        printf("%s\n", environ[i]);
     }
     exit(0);
 }
+
+// This also works
+
+//int main(int argc, char *argv[], char *envp[]) {
+//
+//    for (int i = 0; envp[i] != NULL; i++) {
+//        printf("%s\n", envp[i]);
+//    }
+//    exit(0);
+//}
